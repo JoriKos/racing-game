229f3798eb7d774772886026730abaebb0614171
Epic Gamer <43441605+JoriKos@users.noreply.github.com>
2020-03-03 20:55:49 +0100

    Variabelen en functions

--- RaceGame/Vehicle.h
+++ RaceGame/Vehicle.h
@@ -1,13 +1,28 @@
 #pragma once
+#include "Driver.h"
 #include <string>
+#include <iostream>
+
 class Vehicle
 {
 public:
+	Vehicle();
+	~Vehicle();
 	bool WinChance(float tempSpeed) 
 	{
 
 	}
+	float CalculateStats() 
+	{
 
+	}
+	Driver PickDriver()
+	{
+		for (int i = 0; i < drivers.size(); i++)
+		{
+			std::cout << drivers[i] << std::endl; //Print de array door middel van een for loop
+		}
+	}
 
 
 private:
@@ -16,4 +31,8 @@ private:
 	std::string name;
 	std::string frame;
 	bool door;
+
+protected:
+	std::vector<Driver> drivers = {}; //Voeg drivers toe
+	std::vector<std::string> driverNames = {}; //Voeg drivers toe
 };
